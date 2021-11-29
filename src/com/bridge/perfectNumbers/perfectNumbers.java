0ff7c54059132ee59306e071e1d79de94b27f544
Navneet <navn0306@gmail.com>
2021-11-29 15:29:19 +0530

    ADD: Added the welcome message for project

---
+++ src/com/bridge/perfectNumbers/perfectNumbers.java
@@ -0,0 +1,8 @@
+package com.bridge.perfectNumbers;
+
+public class perfectNumbers {
+    public static void main(String[] args) {
+
+        System.out.println("\n Welcome, this program checks if the given number is a perfect number");
+    }
+}
