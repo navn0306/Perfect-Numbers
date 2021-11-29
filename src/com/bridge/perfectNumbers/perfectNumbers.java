0c38f78eb9e6514d67255817ec5adb01d46ffc8c
Navneet <navn0306@gmail.com>
2021-11-29 15:34:35 +0530

    ADD: Added the program to check if the given number is Perfect number or not

--- src/com/bridge/perfectNumbers/perfectNumbers.java
+++ src/com/bridge/perfectNumbers/perfectNumbers.java
@@ -1,8 +1,27 @@
 package com.bridge.perfectNumbers;
-
-public class perfectNumbers {
-    public static void main(String[] args) {
-
-        System.out.println("\n Welcome, this program checks if the given number is a perfect number");
+import java.util.Scanner;
+public class perfectNumbers
+{
+    public static void main(String arg[])
+    {
+        int n,sum=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        n=sc.nextInt();
+        int i=1;
+        while(i<=n/2)
+        {
+            if(n%i==0)
+            {
+                sum+=i;
+            }
+            i++;
+        }
+        if(sum==n)
+        {
+            System.out.println(n+" is a perfect number");
+        }
+        else
+            System.out.println(n+" is not a  perfect number");
     }
 }
